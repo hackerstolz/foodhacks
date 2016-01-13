19e7f917056e8be1ac9377446068cf20102601bd
Alexander Mueller <alexan.chr.mueller@gmail.com>
2016-01-13 19:52:50 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require( 'gulp' ),
+  plumber = require( 'gulp-plumber' ),
+  watch = require( 'gulp-watch' ),
+  livereload = require( 'gulp-livereload' ),
+  minifycss = require( 'gulp-minify-css' ),
+  jshint = require( 'gulp-jshint' ),
+  stylish = require( 'jshint-stylish' ),
+  uglify = require( 'gulp-uglify' ),
+  rename = require( 'gulp-rename' ),
+  notify = require( 'gulp-notify' ),
+  include = require( 'gulp-include' ),
+  stylus = require('gulp-stylus')
+  sass = require( 'gulp-sass' );
+
+
+var server = require('gulp-server-livereload');
+
+var onError = function( err ) {
+  console.log( 'An error occurred:', err.message );
+  this.emit( 'end' );
+}
+
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(server({
+      livereload: true,
+      directoryListing: false,
+      open: true,
+      fallback: './app/index.html'
+    }));
+});
+
+
+gulp.task( 'stylus', function() {
+  return gulp.src('stylus/main.styl')
+        .pipe(stylus({compress: false, paths: ['stylus']}))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('./app'))
+        .pipe(minifycss())
+        .pipe( rename( { suffix: '.min' } ) )
+        .pipe(gulp.dest('./app'))
+        .pipe(livereload())
+  /*
+    return gulp.src( './scss/style.scss' )
+    .pipe( plumber( { errorHandler: onError } ) )
+    .pipe( sass() )
+    .pipe( gulp.dest( './app' ) )
+    .pipe( minifycss() )
+    .pipe( rename( { suffix: '.min' } ) )
+    .pipe( gulp.dest( './app' ) )
+    .pipe( livereload() );*/
+} );
+
+gulp.task( 'watch', function() {
+  livereload.listen();
+  gulp.watch( './stylus/*.styl', [ 'stylus' ] );
+  gulp.watch( './app/**/*.html' ).on( 'change', function( file ) {
+    livereload.changed( file );
+  } );
+} );
+
+gulp.task( 'default', [ 'stylus', 'watch' ], function() {
+
+} );
